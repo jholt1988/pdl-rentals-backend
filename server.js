7bdf3624b97d3bab101c396bc383d300529c973e
jholt1988 <64242246+jholt1988@users.noreply.github.com>
2025-03-21 10:02:28 -0500

    change deploy.yml

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(helmet());
 app.use(morgan("dev"));
 app.options('*', cors());
-app.use(cors());
+
 
 // Middleware for authentication
 const authenticateToken = (req, res, next) => {
