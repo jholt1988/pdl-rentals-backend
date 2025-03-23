9d6b64fe8301f252d63796b8f037ee3576e88d3a
jholt1988 <64242246+jholt1988@users.noreply.github.com>
2025-03-23 13:33:41 -0500

    fix: update CORS configuration to allow all origins for broader access

--- server.js
+++ server.js
@@ -39,14 +39,8 @@ var corsOptions = {
 }// Corrected code: Set only one allowed origin.
 
 const corsOptions = {
-    origin: "https://pdl-rentals-frontend.vercel.app",
-    credentials: true, //access-control-allow-credentials:
-    optionSuccessStatus: 200,
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
-    allowedHeaders: "Content-Type, Authorization, X-Requested-With, Accept, Origin, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Access-Control-Allow-Methods, Access-Control-Allow-Credentials",
-    exposedHeaders: "Access-Control-Allow-Origin, Access-Control-Allow-Headers, Access-Control-Allow-Methods, Access-Control-Allow-Credentials"
+    origin: "*"
 }
-
 
 
 const app = express();
