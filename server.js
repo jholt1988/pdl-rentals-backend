3ec658022d74c87ea64a3a076e465b464cc7f2fb
jholt1988 <64242246+jholt1988@users.noreply.github.com>
2025-03-23 17:52:03 -0500

    fix: update middleware usage for improved logging and request handling

--- server.js
+++ server.js
@@ -44,11 +44,7 @@ var corsOptions = {
 const app = express();
 app.use(express.json());
 app.use(helmet());
-app.use(morgan());
-
-app.use(bodyParser.json()); 
-app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(morgan("common"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
